7ffdcda013ae0fac3d17574dc705884e2fd51804
Krister Viirsaar <git@krister.ee>
2021-05-27 07:23:06 +0300

    Remove ALL console.log commands. We don't need them anymore.

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-console.log("This message appears in the Dev Console")
 
 const input = document.querySelector('#write')
-console.log(input.id)
 
 input.onkeyup = function(event) {
-    console.log(input.value)
-    console.log(event.key)
     if (event.key === 'Enter') {
-        console.log("Enter was pressed")
         const messagesDiv = document.querySelector('#messages')
-        console.log(messagesDiv.innerHTML)
         const newMessage = '<p>' + input.value + '</p>'
-        console.log(newMessage)
         messagesDiv.innerHTML = messagesDiv.innerHTML + newMessage
         input.value = ''
     }
