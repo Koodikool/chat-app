e3ada5c5ebb5469e05aa31d58b86266d4686699b
Krister Viirsaar <git@krister.ee>
2021-05-27 09:17:48 +0300

    Place the messages onto the page.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 const input = document.querySelector('#write')
 const nameInput = document.querySelector('#name')
+const messagesDiv = document.querySelector('#messages')
 input.focus()
 
 input.onkeyup = function(event) {
     if (event.key === 'Enter') {
-        const messagesDiv = document.querySelector('#messages')
         const newMessage = '<p>' + nameInput.value + ': ' + input.value + '</p>'
         messagesDiv.innerHTML = messagesDiv.innerHTML + newMessage
         input.value = ''
@@ -20,6 +20,7 @@ async function getMessages() {
         console.log(message)
         const messageHTML = '<p>' + message.user + ': ' + message.message + '</p>'
         console.log(messageHTML)
+        messagesDiv.innerHTML = messagesDiv.innerHTML + messageHTML
     }
 }
 getMessages()
