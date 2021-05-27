08fd286aef94ec209ec4c19e95e70954930a439f
Krister Viirsaar <git@krister.ee>
2021-05-27 07:17:10 +0300

    Put the new message into HTML.

--- script.js
+++ script.js
@@ -10,5 +10,7 @@ input.onkeyup = function(event) {
         console.log("Enter was pressed")
         const messagesDiv = document.querySelector('#messages')
         console.log(messagesDiv.innerHTML)
+        const newMessage = '<p>' + input.value + '</p>'
+        console.log(newMessage)
     }
 }
