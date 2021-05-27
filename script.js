fa3bbb19e803e2817be7b72f20a3bfda9f007d66
Krister Viirsaar <git@krister.ee>
2021-05-27 07:41:49 +0300

    Attach username to sent message. So for example 'Alice: Hello everyone!' is shown on the page.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 const input = document.querySelector('#write')
+const nameInput = document.querySelector('#name')
 input.focus()
 
 input.onkeyup = function(event) {
     if (event.key === 'Enter') {
         const messagesDiv = document.querySelector('#messages')
-        const newMessage = '<p>' + input.value + '</p>'
+        const newMessage = '<p>' + nameInput.value + ': ' + input.value + '</p>'
         messagesDiv.innerHTML = messagesDiv.innerHTML + newMessage
         input.value = ''
     }
